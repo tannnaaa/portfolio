8ab4975670d9ae2bd79784e5266fdb72507a2e14
tannnaaa <ekanu20023@yahoo.co.jp>
2022-09-25 14:04:38 +0900

    ページ修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,37 @@
 import * as React from "react"
 import type { HeadFC } from "gatsby"
+import GreetPage from "../components/greeting"
+import ProductPage from "../components/products"
+import SkillPage from "../components/skill"
+const base = require("../components/scss/base.module.scss")
+const page = require("../components/scss/page.module.scss")
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const year = new Date().getFullYear();
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+const IndexPage = () => (
+  <main>
+    <div className={page.top}>
+      <div className={`${base.flex} ${base.container} ${page.row_reverse}`}>
+        <div className={page.line}></div>
+        <div className={page.line}></div>
+      </div>
+    </div>
+    <div className={base.container}>
+      <GreetPage></GreetPage>
+      <ProductPage></ProductPage>
+      <SkillPage></SkillPage>
+      <div className={base.copyright}>
+        <p>copyright {year} created by tana.</p>
+      </div>
+    </div>
+  </main>
+)
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => (
+  <>
+    <title>tana portfolio</title>
+    <link rel="icon" href="data:image/svg+xml;charset=utf8,%20%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20height%3D%2248%22%20width%3D%2248%22%3E%20%3Cpath%20d%3D%22M17.7%2028.85q-1.15%200-1.925-.775Q15%2027.3%2015%2026.15t.775-1.925q.775-.775%201.925-.775t1.925.775q.775.775.775%201.925t-.775%201.925q-.775.775-1.925.775Zm12.65%200q-1.15%200-1.925-.775-.775-.775-.775-1.925t.775-1.925q.775-.775%201.925-.775t1.925.775q.775.775.775%201.925t-.775%201.925q-.775.775-1.925.775ZM24%2041q7.1%200%2012.05-4.95Q41%2031.1%2041%2024q0-1.3-.2-2.55-.2-1.25-.5-2.3-1%20.25-2.175.35-1.175.1-2.425.1-4.85%200-9.175-2-4.325-2-7.375-5.7-1.7%204.05-4.875%207.075Q11.1%2022%207%2023.65V24q0%207.1%204.95%2012.05Q16.9%2041%2024%2041Zm0%203q-4.15%200-7.8-1.575-3.65-1.575-6.35-4.275-2.7-2.7-4.275-6.35Q4%2028.15%204%2024t1.575-7.8Q7.15%2012.55%209.85%209.85q2.7-2.7%206.35-4.275Q19.85%204%2024%204t7.8%201.575q3.65%201.575%206.35%204.275%202.7%202.7%204.275%206.35Q44%2019.85%2044%2024t-1.575%207.8q-1.575%203.65-4.275%206.35-2.7%202.7-6.35%204.275Q28.15%2044%2024%2044ZM19.4%207.65q4.4%205.15%208.125%207.05%203.725%201.9%208.175%201.9%201.2%200%201.9-.05t1.55-.3Q36.9%2012.2%2033.025%209.6%2029.15%207%2024%207q-1.35%200-2.55.2-1.2.2-2.05.45ZM7.45%2020.1q2.4-.9%205.475-4.075Q16%2012.85%2017.3%208.35q-4.35%201.95-6.575%204.975Q8.5%2016.35%207.45%2020.1ZM19.4%207.65Zm-2.1.7Z%22%20fill%3D%22%23acbfbf%22%20%2F%3E%20%3C%2Fsvg%3E"/>
+  </>
+)
